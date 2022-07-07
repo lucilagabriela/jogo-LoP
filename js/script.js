31d846bc2c596831b0f990230cdd48867ba14018
Lucila Costa <lucilagdac@gmail.com>
2022-07-06 23:25:56 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 // utilizar o DOM para pegar o datacell do HTML e guardar em uma const (semelhante à variável)
-const elementosCelula = document.querySelectorAll('[data-cell');
-const board = document.querySelector("[data-board]")
-const mensagemTextoVencedor = document.querySelector('[data-mensagem-vencedor-texto')
-const mensagemVencedor = document.querySelector('[data-mensagem-vencedor-texto]')
-const botaoReiniciar = document.querySelector(['data-botao-reiniciar'])
+const elementsCell = document.querySelectorAll('[data-cell]')
+const game = document.querySelector('[data-game]')
+const winningMessageText =  document.querySelector('[data-winning-message-text]')
+const winningMessage = document.querySelector('[data-winning-message]')
 
-let isCircleTurn;
+let isCircleTurn // verificar se é a vez do círculo
 
-const combinacaoVitorias = [ //em listas/array
+const winningCombinations = [ //em listas/array
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -17,6 +16,87 @@ const combinacaoVitorias = [ //em listas/array
     [0, 4, 8],
     [2, 4, 6],
 ]
+
+const startGame = () => {
+    for (const cell of elementsCell) {
+        cell.addEventListener('click', changeClick, { once: true })
+    }
+
+    isCircleTurn = false
+
+    game.classList.add('x')
+}
+
+const endGame = (empate) => { // empate = draw
+    if (empate) {
+        winningMessageText.innerText = "Empate!"
+    } else {
+        winningMessageText.innerText = isCircleTurn ? "Círculo Venceu!" : "X Venceu!"
+    }
+
+    winningMessage.classList.add("show-winning-message")
+}
+
+//chegar qual é o vencedor
+const isWinner = (currentPlayer) => {
+    // verificar se alguma combinacao é preenchida
+    return winningCombinations.some((combinations) => {
+        return combinations.every((index) => {
+            return elementsCell[index].classList.contains(currentPlayer)
+        })
+    })
+}
+
+const mark = (cell, addClass) => {
+    cell.classList.add(addClass)
+}
+
+const symbolChange = () => {
+    isCircleTurn = !isCircleTurn
+
+    game.classList.remove('circle')
+    game.classList.remove('x')
+
+    if (isCircleTurn) {
+        game.classList.add('circle')
+    } else {
+        game.classList.add('x')
+    }
+}
+
+const changeClick = (e) => {
+    // colocar X ou circle
+    const cell = e.target
+    const addClass = isCircleTurn ? "circle" : "x"
+
+    mark(cell, addClass)
+
+    // verificar por vitória
+    const isWin = isWinner(addClass)
+
+    // verificar por empate
+    if (isWin) {
+        endGame(false)
+    }
+    // mudar símbolo
+    symbolChange()
+} 
+
+for (const cell of elementsCell) {
+    // adicionar evento em cada célula e chama a funcao
+    cell.addEventListener('click', changeClick, { once: true })
+}
+
+startGame()
+
+/*
+const mensagemTextoVencedor = document.querySelector('[data-mensagem-vencedor-texto')
+const mensagemVencedor = document.querySelector('[data-mensagem-vencedor-texto]')
+const botaoReiniciar = document.querySelector(['data-botao-reiniciar'])
+
+let isCircleTurn;
+
+
 
 const iniciarJogo = () => {
     for (const cell of elementosCelula) {
@@ -89,4 +169,4 @@ const handleClick = (e) => { // vai checar se é a vez do circulo
 
 
 
-botaoReiniciar.addEventListener('click', iniciarJogo)
+botaoReiniciar.addEventListener('click', iniciarJogo)*/
